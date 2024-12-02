539f7006d6d38302e85c1b0c1fff86894816e5af
chanli-sudo <aronaleschristianlee@gmail.com>
2024-12-02 15:01:20 +0800

    Add files via upload

--- ATM_Simulator.js
+++ ATM_Simulator.js
@@ -1,1 +1,25 @@
-const text = document.getElementById("text").value
+let balance =  5000;
+let transactionHistory = [];
+ 
+function balanceUpdate () {
+    document.getElementById("balance").textContent = `${balance}`;
+}
+
+    function withdraw(){
+        let amount = prompt("Amount to withdraw");
+        amount = parseFloat(amount);
+    
+        if (isNaN(amount) || amount<= 0 ){
+          alert ( "Please enter valid amount.");
+          return 0;
+        } 
+
+        if ( amount >= 5000){
+            alert (" You have insufficient balance.")
+            return 0;
+        }
+
+       balance -= amount
+       alert (`You have withdrawn ₱${amount}. Your new balance is ₱${balance}. `);
+       balanceUpdate();
+    }
